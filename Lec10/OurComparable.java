1a254106f04941606abc227248f72f4939083228
Amo <1461713136@qq.com>
2022-11-21 15:27:50 +0800

    do some configuration

--- Lec10/OurComparable.java
+++
@@ -1,9 +0,0 @@
-public interface OurComparable {
-
-    /**
-     * Return negative number if this < rhs
-     * Return 0 if this equals to rhs.
-     * Return positive number if this > rhs.
-     */
-    public int compareTo(Object rhs);
-}
